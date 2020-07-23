42cf48e444bf80e68532445739a4bd3f8ced474a
Eric Danielson <eric@egd.im>
2020-07-23 15:12:47 -0700

    Switch api to quart

--- serve.py
+++ serve.py
@@ -2,11 +2,11 @@ import os
 import logging
 import json
 import tekore as tk
-from flask import Flask, escape, request, redirect, url_for, session, render_template
+from quart import Quart, escape, request, redirect, url_for, session, render_template
 
 logging.basicConfig(level=logging.INFO)
 
-app = Flask(__name__)
+app = Quart(__name__)
 app.secret_key = "asdfjdashlgjsad"
 
 client_id, client_secret, redirect_uri = tk.config_from_file("./config.ini")
@@ -19,15 +19,17 @@ scopes = " ".join((
     ))
 
 def write_tokens(user, token):
+    logging.info(f"Asked to write tokens for {user}")
     with open(f"tokens/{user}", "w") as fh:
         fh.write(token.refresh_token)
 
 def get_main_token():
+    logging.info(f"Asked to read main token")
     with open("tokens/main") as fh:
         return fh.read().strip()
 
 @app.route("/main", methods=["GET"])
-def main():
+async def main():
     try:
         token = creds.refresh_user_token(get_main_token())
         spotify = tk.Spotify(token)
@@ -39,7 +41,7 @@ def main():
         return f"{current_user}\n{now_playing}"
 
 @app.route("/main/reset")
-def main_reset():
+async def main_reset():
     logging.info("ok, delete?")
     os.unlink("./tokens/main")
     return "Reset main"
@@ -48,7 +50,7 @@ def main_reset():
 #def main_np():
 
 @app.route("/logout")
-def logout():
+async def logout():
     try:
         token = creds.refresh_user_token(session["r_t"])
         user = tk.Spotify(token).current_user().id
@@ -60,24 +62,29 @@ def logout():
         return "Logged out."
 
 @app.route("/auth", methods=["GET"])
-def auth():
-    logging.warn("Got token: ", request.args)
+async def auth():
+    logging.warning("Got code: %s", request.args["code"])
     token = creds.request_user_token(request.args["code"])
-    logging.warn(token.access_token)
+    logging.warning(token.access_token)
     if request.args["state"] == "main":
+        logging.info("state = main")
         write_tokens("main", token)
         return redirect("/main")
     else:
+        logging.info("state != main")
+        logging.info("refresh token: %s", token.refresh_token)
         set_session_token(token)
+        logging.info(session["r_t"])
         user = tk.Spotify(token).current_user().id
-        write_tokens(user, token)
+        await write_tokens(user, token)
+        logging.info("OK here we are")
         return redirect(f"/")
 
 def set_session_token(token):
     session["r_t"] = token.refresh_token
 
 @app.route("/token", methods=["GET"])
-def token():
+async def token():
     if not session.get("r_t"):
         return "Missing Token", 400
     try:
@@ -88,15 +95,14 @@ def token():
     return token.access_token
 
 @app.route('/', methods=["POST", "GET", "PUT"])
-def do_auth():
+async def do_auth():
+    logging.info("Back here")
+    logging.info(session.get("r_t"))
     if not session.get("r_t"):
         return redirect(creds.user_authorisation_url(scope=scopes, state="follow"))
     else:
-        return render_template("index.html")
-
-@app.route("/player")
-def player():
-    return render_template("index.html")
+        logging.info("Else...")
+        return await render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
