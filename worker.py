5df871a09994318aea162e0f0f6e4206a9c80fad
Eric Danielson <eric@egd.im>
2020-07-23 18:33:43 -0700

    rework worker to add fs checks, more robustness

--- worker.py
+++ worker.py
@@ -22,21 +22,70 @@ class BadToken(Exception):
 class BadClient(Exception):
     pass
 
-def read_tokens():
-    with open(f"{token_dir}/main") as fh:
-        main = fh.read().strip()
-    followers = {}
-    for follower_token in os.listdir(token_dir):
-        if follower_token == "main":
-            continue
-        with open(f"{token_dir}/{follower_token}") as fh:
-            followers[follower_token] = fh.read().strip()
-    return main, followers
+class NoDevices(Exception):
+    pass
 
-def token_path(username):
-    if not (token_dir and username):
-        raise Exception("called for token_path without username or token_dir, avoiding potential CALAMITY")
-    return f"./{token_dir}/{username}"
+# In a real environment we'd be using a data store. In here, we're sharing a dictionary.
+followers = {}
+
+########
+# PLAYBACK
+########
+
+
+async def play_track(user, spotify, track_id, retry=False):
+    try:
+        await spotify.playback_start_tracks([track_id,])
+        return user, True
+    except tk.NotFound:
+        if retry:
+            return user, False
+        user = await setup_follower(user, None)
+        return await play_track(user, spotify, track_id, retry=True)
+
+async def play_to_all(track_id, leader, followers):
+    try:
+        await leader.playback_pause()
+    except:
+        pass
+    await leader.playback_seek(0)
+    results = await asyncio.gather(*[
+        play_track(user, spotify, track_id) for (user, spotify) in followers.items()
+    ])
+    try:
+        await leader.playback_resume()
+    except:
+        logging.exception("Couldn't make user continue. Oh well.")
+    for user, success in results:
+        if not success:
+            logging.info(f"couldn't play track for {user}")
+
+async def stop(spotify):
+    try:
+        await spotify.playback_pause()
+    except:
+        pass
+
+async def stop_all(followers):
+    await asyncio.gather(*[
+        stop(spotify) for spotify in followers.values()
+    ])
+
+#######
+# BASIC USER SETUP
+#######
+async def get_user(token_str):
+    try:
+        token = await refresh_token(token_str)
+        spotify = await get_spotify(token)
+        user = await spotify.current_user()
+    except Exception:
+        logging.exception(f"Could not refresh token for {username}")
+        raise
+    else:
+        display_name = user.display_name
+        logging.info(f"Got client for {display_name}")
+        return display_name, spotify
 
 async def refresh_token(token_str):
     try:
@@ -46,7 +95,6 @@ async def refresh_token(token_str):
     else:
         return token
 
-
 async def get_spotify(token):
     try:
         spotify = tk.Spotify(token, asynchronous=True)
@@ -55,143 +103,96 @@ async def get_spotify(token):
     else:
         return spotify
 
-async def get_main(token_str):
-    try:
-        token = await refresh_token(token_str)
-        spotify = await get_spotify(token)
-        user = await spotify.current_user()
-    except:
-        raise FatalError("Could not get main user")
+async def set_device(spotify):
+    devices = await spotify.playback_devices()
+    devices = [device for device in devices if device.name == "Game Night"]
+    if not devices:
+        raise NoDevices("No valid devices found")
     else:
-        return user.display_name, spotify
+        device = devices[0].id
+        await spotify.playback_transfer(device)
 
-async def setup_follower(username):
-    try:
-        token_str = await store.read_token(username)
-        display_name, spotify = await get_follower(username, token_str)
-    except:
-        logging.exception(f"Failed to get user {username}")
-        return username, None
-    _, success = await set_device(display_name, spotify)
-    if not success:
-        return username, None
-
-async def get_follower(username, token_str):
-    try:
-        token = await refresh_token(token_str)
-        spotify = await get_spotify(token)
-        user = await spotify.current_user()
-    except Exception:
-        logging.exception(f"Could not refresh token for {username}, skipping")
-        return None, None
-    else:
-        display_name = user.display_name
-        logging.info(f"Got client for {display_name}")
-        return display_name, spotify
-
-async def get_followers(followers, main_token):
-    followers = await asyncio.gather(*[
-        get_follower(username, token) for username, token in followers.items()
-                if token != main_token])
-    return {user:token for user, token in followers if user is not None}
-
-async def get_clients():
-    logging.info("Hello")
-    main_token_str, follower_token_strings = read_tokens()
-    name, main = await get_main(main_token_str)
-    logging.info(f"Got main user: {name}")
-    followers = await get_followers(follower_token_strings, main_token_str)
-    logging.info(f"Got {len(followers)} followers:")
-    for follower in followers:
-        logging.info(f" - {follower}")
-    return main, followers
-
-async def play_track(user, spotify, track_id, retry=False):
-    try:
-        await spotify.playback_start_tracks([track_id,])
-        return user, True
-    except tk.NotFound:
-        if retry:
-            return user, False
-        _, updated = await set_device("", spotify)
-        if updated:
-            return await play_track(user, spotify, track_id, retry=True)
-        else:
-            return user, False
-
-async def sync(track_id, main, followers):
-    try:
-        await main.playback_pause()
-    except:
-        pass
-    await main.playback_seek(0)
-    results = await asyncio.gather(*[
-        play_track(user, spotify, track_id) for (user, spotify) in followers.items()
-    ])
-    try:
-        await main.playback_resume()
-    except:
-        logging.exception("Couldn't make user continue. Oh well.")
-    for user, success in results:
-        if not success:
-            logging.info(f"couldn't play track for {user}")
-
+#######
+# SYNC OPERATIONS
+#######
 async def get_current_track(spotify):
     current = await spotify.playback_currently_playing()
     if not current:
         return None, None, False
     else:
-        return current.item.name, current.item.id, current.is_playing
+        artists = ", ".join([a.name for a in current.item.artists])
+        song_name = f"{current.item.name} - {artists}"
+        return song_name, current.item.id, current.is_playing
 
-async def set_device(user, spotify):
-    devices = await spotify.playback_devices()
-    devices = [device for device in devices if device.name == "Game Night"]
-    if not devices:
-        return user, False
+async def check_new(leader, current_id, current_playing):
+    new_name, new_id, new_playing = await get_current_track(leader)
+    changed = (new_id != current_id or new_playing != current_playing)
+    if changed:
+        logging.info(f"New track: {new_name}")
+        await store.write_song(new_name if new_name else "Not Playing")
+    return changed, new_id, new_playing
+
+async def sync(leader, followers, song_id, playing):
+    if song_id == None or playing == False:
+        logging.info("leader stopped.")
+        await stop_all(followers)
     else:
-        device = devices[0].id
-        await spotify.playback_transfer(device)
-        return user, True
+        logging.info(f"Got new track: {song_id}")
+        await play_to_all(song_id, leader, followers)
 
-async def setup_followers(followers):
-    tasks = []
-    for follower, spotify in followers.items():
-        tasks.append(set_device(follower, spotify))
-    results = await asyncio.gather(*tasks)
-    for (user, success) in results:
-        if not success:
-            logging.info(f"Could not set device for {user}")
-            del(followers[user])
-
-async def stop(spotify):
+#######
+# Leader and Follower Setup
+#######
+async def setup_follower(username, spotify):
     try:
-        await spotify.playback_pause()
-    except:
-        pass
+        token_str = await store.get_token(username)
+        if spotify is not None and token_str == spotify.token.refresh_token:
+            return username, spotify
+        display_name, spotify = await get_user(token_str) 
+        await set_device(spotify)
+        return username, spotify
+    except Exception as err:
+        logging.exception("Could not set up user %s", username)
+        return username, None
 
+async def check_followers(followers):
+    users = [u for u in await store.list_tokens() if u != "main"]
+    loaded_users = await asyncio.gather(*[
+        setup_follower(username, followers.get(username))
+        for username in users])
+    followers = {user:spotify for user, spotify in loaded_users if spotify is not None}
+    logging.info(followers)
+    return followers
 
-async def stop_all(followers):
-    await asyncio.gather(*[
-        stop(spotify) for spotify in followers.values()
-    ])
+async def check_leader(leader):
+    if not await store.have_token("main"):
+        return None
+    try:
+        token = await store.get_token("main")
+        if leader is None or token != leader.token.refresh_token:
+            username, leader = await get_user(token)
+            logging.info(f"Got leader user: {username}")
+        return leader
+    except Exception as err:
+        logging.exception("Error getting leader user.")
+        return None
 
-
+#######
+# And now the magic.
+#######
 async def main():
-    main, followers = await get_clients()
-    await setup_followers(followers)
-    current_name, current_id, current_playing = await get_current_track(main)
+    leader = None
+    followers = dict()
+    song_id, playing = None, False
     while True:
         await asyncio.sleep(2)
-        new_name, new_id, new_playing = await get_current_track(main)
-        if new_id != current_id or new_playing != current_playing:
-            if new_id == None or new_playing == False:
-                logging.info("Main stopped.")
-                await stop_all(followers)
-            else:
-                logging.info(f"Got new track: {new_name}")
-                await sync(new_id, main, followers)
-            current_name, current_id, current_playing = new_name, new_id, new_playing
-
+        leader = await check_leader(leader)
+        if not leader:
+            continue
+        changed, song_id, playing = await check_new(leader, song_id, playing)
+        if changed:
+            followers = await check_followers(followers)
+            await sync(leader, followers, song_id, playing)
 
 if __name__ == "__main__":
     asyncio.run(main())
